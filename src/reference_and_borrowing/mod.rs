ad551af967b1ad53ad00fc3eb4c9ac3b5ab4cdb7
Allyn Ralf Ledesma <110903853+allyn8444@users.noreply.github.com>
2025-01-24 01:33:26 +0800

    added new topic -> reference and borrowing (unfinished)

---
+++ src/reference_and_borrowing/mod.rs
@@ -0,0 +1,46 @@
+#![allow(unused_variables)] // allow unused variables
+#![allow(dead_code)] // allow unused functions
+
+pub fn run(){
+    println!("Reference and Borrowing 5.2 Topic");
+}
+
+/*
+
+
+
+
+fn main() {
+   let x = 5;
+   // Fill the blank
+   let p = &x; // use ampersand (&) to get memory address of a value
+
+   println!("the memory address of x is {:p}", p); // One possible output: 0x16fa3ac84
+}
+
+
+
+
+fn main() {
+    let x = 5;
+    let y = &x;
+
+    // Modify this line only
+    assert_eq!(5, *y); // use asterisk (*) to dereference a value (the process of accessing or changing the value stored in a memory address that a pointer points to)
+
+    println!("Success!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
