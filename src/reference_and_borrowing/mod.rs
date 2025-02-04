a4039428ade1d483d34e589aaeff054952e52009
Allyn Ralf Ledesma <110903853+allyn8444@users.noreply.github.com>
2025-02-05 01:12:06 +0800

    small comment changes (status: finished)

--- src/reference_and_borrowing/mod.rs
+++ src/reference_and_borrowing/mod.rs
@@ -198,6 +198,6 @@ fn rb11() {
 
     // Add one line below to make a compiler error: cannot borrow `s` as mutable more than once at a time
     // You can't use r1 and r2 at the same 
-    // Solution:
-    println!("{}",r1); 
+    // Solution: (uncomment it)
+    // println!("{}",r1); 
 }
