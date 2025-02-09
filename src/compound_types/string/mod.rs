54e841d2a797543a6447820123c644df80072b9b
Allyn Ralf Ledesma <110903853+allyn8444@users.noreply.github.com>
2025-02-09 23:57:54 +0800

    continued exercises (status: still unfinished)

--- src/compound_types/string/mod.rs
+++ src/compound_types/string/mod.rs
@@ -12,7 +12,11 @@ pub fn run_string() {
     // s3();
     // s4();
     // s5();
-    s6();
+    // s6();
+    // s7();
+    // s8();
+    // s9();
+    // s10();
 }
 
 
@@ -87,3 +91,75 @@ fn s6() {
     println!("Success! - 6");
 }
 
+
+
+// Fix error with at least two solutions
+fn s7() {
+    let s: &str = "hello, world"; //Solution: either change to &str or String type
+    greetings(s);
+
+    fn greetings(s:  &str) { //Solution: either change to &str or String type
+        println!("{}", s)
+    }
+}
+
+
+
+// Use two approaches to fix the error and without adding a new line
+fn s8() {
+    // let s = "hello, world".to_string(); // This makes it a String type
+    // let s1: String = s; // Solution: change &str to String type
+
+    let s : &str= "hello, world"; //Solution 2: remove .to_string() to make it &str
+    let s1: &str = s; 
+
+    println!("Success! - 8");
+}
+
+
+
+// AN EXAMPLE, NOT AN EXERCISE
+fn s9() {
+    // You can use escapes to write bytes by their hexadecimal values
+    // Fill the blank below to show "I'm writing Rust"
+    let byte_escape = "I'm writing Ru\x73t!"; // \x73 is a byte escape sequence representing the ASCII character 's', 
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    // ...Or Unicode code points.
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!("Unicode character {} (U+211D) is called {}",
+                unicode_codepoint, character_name );
+
+    let long_string = "String literals
+                        can span multiple lines.
+                        The linebreak and indentation here \
+                         can be escaped too!";
+    println!("{}", long_string);
+}
+
+
+
+
+/* Fill in the blank and fix the errors */
+fn s10() {
+    let raw_str = "Escapes don't work here: \x3F \u{211D}"; // remove the r 
+    // Modify above line to make it work
+    assert_eq!(raw_str, "Escapes don't work here: ? ℝ");
+
+    // If you need quotes in a raw string, add a pair of #s
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    // If you need "# in your string, just use more #s in the delimiter.
+    // You can use up to 65535 #s.
+    let delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", delimiter);
+
+    // Fill the blank
+    let long_delimiter = "Hello, \"##\""; // copy the delimiter above in assert_eq
+    assert_eq!(long_delimiter, "Hello, \"##\"");
+
+    println!("Success!");
+}
