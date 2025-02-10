fef66589972767c65efc92dbd00d7705666fe3cc
Allyn Ralf Ledesma <110903853+allyn8444@users.noreply.github.com>
2025-02-10 23:32:59 +0800

    re-exported slice submodule for convenient use

--- src/compound_types/mod.rs
+++ src/compound_types/mod.rs
@@ -3,7 +3,9 @@
 // Declare submodule
 pub mod string;
 pub mod array;
+pub mod slice;
 
 // Re-export for convenient use
 pub use string::run_string; // Bring into scope of main.rs
 pub use array::run_array;
+pub use slice::run_slice;
