d86c56b723b13c8fe26c85498306be0f2a8c5d6b
Allyn Ralf Ledesma <110903853+allyn8444@users.noreply.github.com>
2025-02-11 23:37:23 +0800

    started topic 6.2 Compound Types: Array

--- src/compound_types/array/mod.rs
+++ src/compound_types/array/mod.rs
@@ -8,5 +8,34 @@ pub fn run_array(){
 
     // =============== Exercises =============== //
 
+    // ar1();
+    ar2();
+}
 
+
+fn ar1() {
+    // Fill the blank with proper array type
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    // Solution: [values_type; length]
+
+    // Modify the code below to make it work
+    assert!(arr.len() == 5); // turn 4 into 5
+
+    println!("Success! - 1");
+}
+
+
+
+// WATCH VIDEO
+fn ar2() {
+    // We can ignore parts of the array type or even the whole type, let the compiler infer it for us
+    let arr0 = [1, 2, 3];
+    let arr: [_; 3] = ['a', 'b', 'c'];
+    
+    // Fill the blank
+    // Arrays are stack allocated, `std::mem::size_of_val` returns the bytes which an array occupies
+    // A char takes 4 bytes in Rust: Unicode char
+    assert!(std::mem::size_of_val(&arr) == __);
+
+    println!("Success!");
 }
