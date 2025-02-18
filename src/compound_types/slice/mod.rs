e0c0e589591c1724f46a0762136f442f0eca37f1
Allyn Ralf Ledesma <110903853+allyn8444@users.noreply.github.com>
2025-02-19 00:46:52 +0800

    FINISHED compound types: slice

--- src/compound_types/slice/mod.rs
+++ src/compound_types/slice/mod.rs
@@ -11,7 +11,10 @@ pub fn run_slice(){
 
     // sl1();
     // sl2();
-    sl3();
+    // sl3();
+    // sl4();
+    // sl5();
+    sl6();
 }
 
 /*
@@ -68,4 +71,56 @@ fn sl3() {
     assert_eq!(slice, &[2, 3, 4]);
 
     println!("Success! - 3");
+}
+
+
+
+fn sl4() {
+    let s: String = String::from("hello");
+
+    let slice1:&str = &s[0..2];
+    // Fill the blank to make the code work, DON'T USE 0..2 again
+    let slice2 = &s[..2];
+    // Solution: &s[..2]
+    //  we can omit the zero (it is understandable that it will start at index zero)
+
+    assert_eq!(slice1, slice2);
+
+    println!("Success! - 4");
+}
+
+
+
+fn sl5() {
+    let s = "你好，世界";
+    // Modify this line to make the code work
+    let slice = &s[..3]; // Solution: change 2 to 3
+ 
+    assert!(slice == "你");
+
+    println!("Success! - 5");
+}
+
+
+// Fix errors
+fn sl6() {
+    let mut s = String::from("hello world");
+
+    // Here, &s is `&String` type, but `first_letter` needs a `&str` type.
+    // It works because `&String` can be implicitly converted to `&str. If you want to know more, this is called `Deref coercion`. 
+    let letter = first_letter(&s);
+
+    //  s.clear(); // error! 
+     // Solution: put this after the println
+     // cant use before the println since we are using the value for the `letter`
+
+    println!("the first letter is: {}", letter);
+    
+    s.clear();
+
+
+    fn first_letter(s: &str) -> &str {
+        &s[..1]
+    }
+
 }
